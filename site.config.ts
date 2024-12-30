a2ea91223d7cf28bc985c11ec0eb8d9aa8392059
HONGDA HUANG <46312302+icarsone@users.noreply.github.com>
2024-12-30 23:43:53 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -10,16 +10,16 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'carson',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'hongda.fun',
   author: 'carson',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '',
+  github: '',
+  linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
