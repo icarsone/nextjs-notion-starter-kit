0837e29951885c3edd8975eff70487eb145cd02f
HONGDA HUANG <46312302+icarsone@users.noreply.github.com>
2024-12-30 17:16:59 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'carson',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'carson',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
